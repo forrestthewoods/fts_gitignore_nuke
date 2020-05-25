70cbd89a5ab5bea49d1190cb402bd33367850067
Forrest Smith <forrestthewoods@gmail.com>
2020-05-25 14:10:23 -0700

    Working ignores

---
+++ src/immutable_stack.rs
@@ -0,0 +1,64 @@
+use std::sync::Arc;
+
+#[derive(Clone)]
+pub(crate) struct ImmutableStack<T> {
+    node: Link<T>,
+}
+
+type Link<T> = Option<Arc<Node<T>>>;
+
+pub(crate) struct Node<T> {
+    elem: T,
+    next: Link<T>,
+}
+
+impl<T> ImmutableStack<T> {
+    pub fn new() -> Self {
+        ImmutableStack { node: None }
+    }
+
+    pub fn push(&self, elem: T) -> ImmutableStack<T> {
+        ImmutableStack { 
+            node: Some(
+                Arc::new(
+                    Node {
+                        elem: elem,
+                        next: self.node.clone(),
+                    }
+                )
+            )
+        }
+    }
+
+    pub fn iter(&self) -> Iter<'_, T> {
+        Iter { next: self.node.as_ref().map(|node| &**node) }
+    }
+}
+
+impl<T> Drop for ImmutableStack<T> {
+    fn drop(&mut self) {
+        let mut iter = self.node.take();
+        while let Some(node) = iter {
+            if let Ok(mut node) = Arc::try_unwrap(node) {
+                iter = node.next.take();
+            } else {
+                break;
+            }
+        }
+    }
+}
+
+pub(crate) struct Iter<'a, T> {
+    next: Option<&'a Node<T>>,
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.map(|node| {
+            self.next = node.next.as_ref().map(|node| &**node);
+            &node.elem
+        })
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,185 +1,93 @@
 use anyhow::anyhow;
 use ignore::gitignore::{Gitignore, GitignoreBuilder};
 use std::{env, fs};
-use std::sync::Arc;
-/*
-struct IgnoreIter<'a> {
-    _gitignore : std::rc::Rc<Gitignore>,
-    _returned : bool
-}
 
-impl<'a> Iterator for IgnoreIter<'a> {
-    type Item = &'a Gitignore;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.returned {
-            None
-        } else {
-            *self._gitignore
-        }
-    }
-}
-*/
-
-/*
-struct IgnoreChainNode {
-    gitignore : Arc<Gitignore>,
-    prev : Option<Arc<IgnoreChainNode>>
-}
-
-impl IgnoreChainNode {
-    fn new(gitignore: Arc<Gitignore>) -> Self {
-        Self { 
-            gitignore, 
-            prev: None 
-        }
-    }
-}
-*/
-
-
-#[derive(Clone)]
-struct IgnoreChainIter {
-    inner: Option<(Arc<Gitignore>, Arc<IgnoreChainIter>)>
-}
-
-impl IgnoreChainIter {
-    fn empty() -> Arc<Self> {
-        Arc::new(
-            Self { 
-                inner : None
-            }
-        )
-    }
-
-    fn chain(ignore: Arc<Gitignore>, prev: Arc<IgnoreChainIter>) -> Arc<IgnoreChainIter> {
-        Arc::new(
-            Self {
-                inner: Some((ignore, prev))
-            }
-        )
-    }
-}
-
-impl Iterator for IgnoreChainIter {
-    type Item = Arc<Gitignore>;
-
-    fn next(&mut self) -> Option<Arc<Gitignore>> {
-        match self.inner.clone() {
-            Some((ignore, prev)) => {
-                self.inner = prev.inner.clone();
-                Some(ignore.clone())
-            },
-            None => None,
-        }
-    }
-}
+mod immutable_stack;
+use immutable_stack::ImmutableStack;
 
 fn main() -> anyhow::Result<()> {
     let current_dir = env::current_dir()?;
     println!("Starting Dir: [{:?}]", current_dir);
     let current_dir : std::path::PathBuf = "c:/source_control".into();
-    
-    // Root ignore (empty)
-    let mut ignore_chain = IgnoreChainIter::empty();
 
+    // Root ignore (empty)
+    let ignore_stack = ImmutableStack::new();
+    let mut ignore_tip = ignore_stack.clone();
+    
     // Add global ignore (if exists)
     let (global_ignore, err) = GitignoreBuilder::new(current_dir.clone()).build_global();
     if err.is_none() && global_ignore.num_ignores() > 0 {
-        let arc_global_ignore = Arc::new(global_ignore);
-        ignore_chain = IgnoreChainIter::chain(arc_global_ignore, ignore_chain);
+        ignore_tip = ignore_stack.push(global_ignore).clone();
     }
 
     // List of all ignored paths
-    let mut ignored : Vec<std::path::PathBuf> = Default::default();
+    let mut ignored_paths : Vec<std::path::PathBuf> = Default::default();
 
     // Queue of paths to consider
-    let mut queue : std::collections::VecDeque<(Arc<IgnoreChainIter>, std::path::PathBuf)> = Default::default();
-    queue.push_back((ignore_chain, current_dir));
+    let mut queue : std::collections::VecDeque<(ImmutableStack<Gitignore>, std::path::PathBuf)> = Default::default();
+    queue.push_back((ignore_tip, current_dir));
+
 
     // Process all paths
     while !queue.is_empty() {
-        let (ignore_chain, entry) = queue.pop_front().unwrap();
+        let (mut ignore_tip, entry) = queue.pop_front().unwrap();
         assert!(std::fs::metadata(&entry)?.is_dir());
 
-        //println!("Dir: {:?}", entry);
-
+        // Dirs must be deferred so they can consider potential ignore files
         let mut dirs : Vec<std::path::PathBuf> = Default::default();
-        let mut new_ignore_chain = ignore_chain.clone();
 
+        // Process each child in directory
         for child in fs::read_dir(entry)? {
             let child_path = child?.path();
             let child_meta = std::fs::metadata(&child_path)?;
 
+            // Child is file
             if child_meta.is_file() {
+                // Child is ignorefile. Push it onto ignore stack
                 if child_path.file_name().unwrap() == ".gitignore" {
+                    // Parse new .gitignore
                     let parent_path = child_path.parent().ok_or(anyhow!("Failed to get parent for [{:?}]", child_path))?;
                     let mut ignore_builder = ignore::gitignore::GitignoreBuilder::new(parent_path);
                     ignore_builder.add(child_path);
-                    let new_ignore = Arc::new(ignore_builder.build()?);
-                    new_ignore_chain = IgnoreChainIter::chain(new_ignore, ignore_chain.clone());
+                    let new_ignore = ignore_builder.build()?;
+                    ignore_tip = ignore_tip.push(new_ignore);
                 } else {
-                    let is_ignored = ignore_chain.clone()
+                    // Child is file. Perform ignore test.
+                    let is_ignored = ignore_tip.iter()
                         .map(|i| i.matched(&child_path, true))
-                        .any(|m| {
-                            if m.is_ignore() {
-                                println!("{:?}", m.inner().unwrap());
-                                true
-                            } else {
-                                false
-                            }
-                        });
+                        .any(|m| m.is_ignore());
 
                     if is_ignored {
-                        ignored.push(child_path);
+                        ignored_paths.push(child_path);
                     }
                 }
             } else {
+                // Child is directory. Add to deferred dirs list
                 dirs.push(child_path);
             }
         }
 
+        // Process directories
         for dir in dirs.into_iter() {
 
-            let is_ignored = new_ignore_chain.clone()
+            // Check if ignored
+            let is_ignored = ignore_tip.iter()
                 .map(|i| i.matched(&dir, true))
                 .any(|m| m.is_ignore());
 
-
             if is_ignored {
-                ignored.push(dir);
+                ignored_paths.push(dir);
             } else {
-                queue.push_back((new_ignore_chain.clone(), dir));
+                queue.push_back((ignore_tip.clone(), dir));
             }
         }
     }
 
-    println!("Ignored");
-    for path in ignored.into_iter() {
+    // Print all ignored content
+    println!("Ignored:");
+    for path in ignored_paths.into_iter() {
         println!("  {:?}", path);
     }
-
-        /*
-    for entry in fs::read_dir(current_dir)? {
-        let entry = entry?;
-        let path = entry.path();
-        println!("{:?}", path);
-
-        let metadata = fs::metadata(&path)?;
-        let last_modified = metadata.modified()?.elapsed()?.as_secs();
-
-        if last_modified < 24 * 3600 && metadata.is_file() {
-            //let filename = path.file_name().ok_or(anyhow!("uh oh"))?;
-            println!(
-                "Last modified: {:?} seconds, is read only: {:?}, size: {:?} bytes, filename: {:?}",
-                last_modified,
-                metadata.permissions().readonly(),
-                metadata.len(),
-                path.file_name().ok_or(anyhow!("No filename"))?
-            );
-        }
-    }
-        */
 
     Ok(())
 }
