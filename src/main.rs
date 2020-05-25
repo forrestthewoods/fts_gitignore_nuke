7f47ed07d40d2f716ad32306ca31951e74a58683
Forrest Smith <forrestthewoods@gmail.com>
2020-05-24 17:52:10 -0700

    Cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,56 @@
 use std::{env, fs};
-//use anyhow::anyhow;
+use anyhow::anyhow;
 
 fn main() -> anyhow::Result<()> {
     let current_dir = env::current_dir()?;
-    println!(
-        "Entries modified in the last 24 hours in {:?}:",
-        current_dir
-    );
+    println!("Starting Dir: [{:?}]", current_dir);
 
     let mut queue : std::collections::VecDeque<std::path::PathBuf> = Default::default();
-    queue.push_front(current_dir.clone());
+    queue.push_back(current_dir.clone());
+
+    //let mut dirs : Vec<std::path::PathBuf> = Default::default();
+    let mut ignores : Vec<ignore::gitignore::Gitignore> = Default::default();
 
     while !queue.is_empty() {
-        let entry = queue.pop_back().unwrap();
+        let entry = queue.pop_front().unwrap();
         assert!(std::fs::metadata(&entry)?.is_dir());
 
-        println!("{:?}", entry);
+        println!("Dir: {:?}", entry);
+
+        let mut dirs : Vec<std::path::PathBuf> = Default::default();
 
         for child in fs::read_dir(entry)? {
             let child_path = child?.path();
             let child_meta = std::fs::metadata(&child_path)?;
 
             if child_meta.is_file() {
-                println!("{:?}", child_path);
+                println!("File: {:?}", child_path);
+
+                if child_path.file_name().unwrap() == ".gitignore" {
+                    println!("!!! IGNORE: {:?}", child_path);
+                    let parent_path = child_path.parent().ok_or(anyhow!("Failed to get parent for [{:?}]", child_path))?;
+                    let mut ignore_builder = ignore::gitignore::GitignoreBuilder::new(parent_path);
+                    ignore_builder.add(child_path);
+                    ignores.push(ignore_builder.build()?);
+
+                    // TODO: Make gitignore builder
+                }
             } else {
-                queue.push_front(child_path);
+                dirs.push(child_path);
+            }
+        }
+
+        for dir in dirs.into_iter() {
+
+            let ignored = ignores.iter()
+                .map(|i| i.matched(&dir, true))
+                .any(|m| m.is_ignore());
+
+            println!("Ignored?: [{}] [{:?}]", ignored, dir);
+
+            
+            if !ignored {
+                //queue.push_back(dir);
             }
         }
     }
