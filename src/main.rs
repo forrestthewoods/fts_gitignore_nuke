c98b69650447b5f156c9c1b5efab305e4d7841d8
Forrest Smith <forrestthewoods@gmail.com>
2020-05-25 14:42:30 -0700

    Working with printed file sizes

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,61 @@
 use anyhow::anyhow;
 use ignore::gitignore::{Gitignore, GitignoreBuilder};
+use itertools::Itertools;
 use std::{env, fs};
 
 mod immutable_stack;
 use immutable_stack::ImmutableStack;
+
+fn dir_size(path: std::path::PathBuf) -> anyhow::Result<u64> {
+    // Input path may be file
+    let meta = fs::metadata(&path)?;
+    if meta.is_file() {
+        return Ok(meta.len());
+    }
+    
+    // Recursively compute pathsize
+    let mut result = 0;
+
+    let mut dirs = vec![path];
+    while !dirs.is_empty() {
+        let dir = dirs.pop().unwrap();
+
+        // Process each element in dir
+        for child in fs::read_dir(dir)? {
+            let child_path = child?.path();
+            let meta = fs::metadata(&child_path)?;
+            if meta.is_file() {
+                result += meta.len();
+            } else {
+                dirs.push(child_path);
+            }
+        }
+    }
+
+    Ok(result)
+}
+
+fn pretty_bytes(orig_amount: u64) -> String {
+    let mut amount = orig_amount;
+    let mut order = 0;
+    while amount > 1000 {
+        amount /= 1000;
+        order += 1;
+    }
+
+    match order {
+        0 => format!("{} b", amount),
+        1 => format!("{} Kb", amount),
+        2 => format!("{} Mb", amount),
+        3 => format!("{} Gb", amount),
+        4 => format!("{} Tb", amount),
+        5 => format!("{} Pb", amount),
+        6 => format!("{} Exa", amount),
+        7 => format!("{} Zetta", amount),
+        8 => format!("{} Yotta", amount),
+        _ => format!("{}", orig_amount)
+    }
+}
 
 fn main() -> anyhow::Result<()> {
     let current_dir = env::current_dir()?;
@@ -85,9 +137,15 @@ fn main() -> anyhow::Result<()> {
 
     // Print all ignored content
     println!("Ignored:");
-    for path in ignored_paths.into_iter() {
-        println!("  {:?}", path);
-    }
+    //for path in ignored_paths
+    //    .into_iter() 
+    ignored_paths.into_iter()
+        .map(|dir| (dir_size(dir.clone()), dir))
+        .filter_map(|(bytes, dir)| if let Ok(bytes) = bytes { Some((bytes, dir)) } else { None })
+        .sorted_by_key(|kvp| kvp.0)
+        .for_each(|(bytes, path)| {
+            println!("  {:10} {:?}", pretty_bytes(bytes), path);
+        });
 
     Ok(())
 }
