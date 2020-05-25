c08a2eac35b7ea00c491026d22b50072edab6ab1
Forrest Smith <forrestthewoods@gmail.com>
2020-05-25 16:11:12 -0700

    Almost deleting for real

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use anyhow::{anyhow, Context};
+use anyhow::anyhow;
+use clap::{Arg, App};
 use ignore::gitignore::{Gitignore, GitignoreBuilder};
 use itertools::Itertools;
 use num_format::{Locale, ToFormattedString};
@@ -7,6 +8,7 @@ use std::{env, fs};
 mod immutable_stack;
 use immutable_stack::ImmutableStack;
 
+// Recursively calculate size of all files within a single directory
 fn dir_size(path: std::path::PathBuf) -> anyhow::Result<u64> {
     // Input path may be file
     let meta = fs::metadata(&path)?;
@@ -26,8 +28,10 @@ fn dir_size(path: std::path::PathBuf) -> anyhow::Result<u64> {
             let child_path = child?.path();
             let meta = fs::metadata(&child_path)?;
             if meta.is_file() {
+                // Add file sizes to result
                 result += meta.len();
             } else {
+                // Add directories to queue for processing
                 dirs.push(child_path);
             }
         }
@@ -36,6 +40,7 @@ fn dir_size(path: std::path::PathBuf) -> anyhow::Result<u64> {
     Ok(result)
 }
 
+// Print u64 bytes value as a suffixed string
 fn pretty_bytes(orig_amount: u64) -> String {
     let mut amount = orig_amount;
     let mut order = 0;
@@ -59,16 +64,39 @@ fn pretty_bytes(orig_amount: u64) -> String {
 }
 
 fn main() -> anyhow::Result<()> {
-    let current_dir = env::current_dir().with_context(|| format!("Failed to evaluate env::current_dir"))?;
-    println!("Starting Dir: [{:?}]", current_dir);
-    let current_dir : std::path::PathBuf = "c:/temp".into();
 
-    // Root ignore (empty)
+    // Parse cmdline args
+    let matches = App::new("☢️ fts_gitignore_nuke ☢️")
+        .version(".1")
+        .author("Forrest S. <forrestthewoods@gmail.com>")
+        .about("Deletes files hidden by .gitignore files")
+        .arg(Arg::with_name("directory")
+            .short("d")
+            .long("directory")
+            .value_name("DIRECTORY")
+            .help("Root directory to start search")
+        )
+        .get_matches();
+    
+    // Determine starting dir
+    let starting_dir : std::path::PathBuf = match matches.value_of_os("directory") {
+        Some(path) => path.into(),
+        None => env::current_dir()?
+    };
+    
+    // Verify starting dir is valid
+    if !starting_dir.exists() {
+        return Err(anyhow!("Directory [{:?}] does not exist", starting_dir));
+    } else if !starting_dir.is_dir() {
+        return Err(anyhow!("[{:?}] is not a directory", starting_dir));
+    }
+
+    // Start .gitignore stack with an empty root
     let ignore_stack = ImmutableStack::new();
     let mut ignore_tip = ignore_stack.clone();
     
     // Add global ignore (if exists)
-    let (global_ignore, err) = GitignoreBuilder::new(current_dir.clone()).build_global();
+    let (global_ignore, err) = GitignoreBuilder::new(starting_dir.clone()).build_global();
     if err.is_none() && global_ignore.num_ignores() > 0 {
         ignore_tip = ignore_stack.push(global_ignore).clone();
     }
@@ -78,19 +106,23 @@ fn main() -> anyhow::Result<()> {
 
     // Queue of paths to consider
     let mut queue : std::collections::VecDeque<(ImmutableStack<Gitignore>, std::path::PathBuf)> = Default::default();
-    queue.push_back((ignore_tip, current_dir));
+    queue.push_back((ignore_tip, starting_dir));
 
 
     // Process all paths
     while !queue.is_empty() {
         let (mut ignore_tip, entry) = queue.pop_front().unwrap();
-        assert!(std::fs::metadata(&entry).with_context(|| format!("Could not access metadata: [{:?}]", &entry))?.is_dir());
 
         // Dirs must be deferred so they can consider potential ignore files
         let mut dirs : Vec<std::path::PathBuf> = Default::default();
 
         // Process each child in directory
-        for child in fs::read_dir(entry)? {
+        let read_dir = match fs::read_dir(entry) {
+            Ok(inner) => inner,
+            Err(_) => continue,
+        };
+
+        for child in read_dir {
             let child_path = child?.path();
             let child_meta = match std::fs::metadata(&child_path) {
                 Ok(inner) => inner,
@@ -153,6 +185,20 @@ fn main() -> anyhow::Result<()> {
             println!("  {:10} {:?}", pretty_bytes(bytes), path);
         });
     println!("Total Bytes: {}", total_bytes.to_formatted_string(&Locale::en));
+
+    // Verify nuke
+    const NUKE_STRING : &str = "NUKE";
+    println!("\nDo you wish to delete? This action can not be undone!\nType {} to proceed:", NUKE_STRING);
+    
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input)?;
+    let trimmed_input = input.trim();
+    if trimmed_input == NUKE_STRING {
+        println!("\n☢️☢️☢️ Nuclear launch detected ☢️☢️☢️");
+        println!("Nuclear deletion complete.");
+    } else {
+        println!("Nuclear launch aborted. Input was [{}] but must exactly match [{}] to irrevocably nuke.", trimmed_input, NUKE_STRING);
+    }
 
     Ok(())
 }
