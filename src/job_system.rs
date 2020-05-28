37a6cbe00d4dcaba5ad3bcf856c9418438c2ccc5
Forrest Smith <forrestthewoods@gmail.com>
2020-05-28 02:10:35 -0700

    More recursion. But added paths I didn't need to. Oops.

--- src/job_system.rs
+++ src/job_system.rs
@@ -1,64 +1,79 @@
 use crossbeam_deque::{Injector, Stealer, Worker};
+use std::sync::Arc;
+use std::sync::atomic::{AtomicUsize, Ordering};
 
-pub fn run_job<IN,OUT,JOB>(initial: Vec<IN>, job: JOB, num_workers: usize) -> Vec<OUT> 
+pub fn run_recursive_job<IN,OUT,JOB>(initial: Vec<IN>, job: JOB, num_workers: usize) -> Vec<OUT> 
 where 
     IN: Send,
     OUT: Send,
     JOB: Fn(IN, &Worker<IN>) -> Option<OUT> + Clone + Send,
-{
+{   
+    // Create crossbeam_deque injector/worker/stealers
     let injector = Injector::new();
     let workers : Vec<_> = (0..num_workers).map(|_| Worker::new_lifo()).collect();
     let stealers : Vec<_> = workers.iter().map(|w| w.stealer()).collect();
-    let active_counter = crate::ActiveCounter::new();
+    let active_counter = ActiveCounter::new();
 
-    // Seed injector
+    // Seed injector with initial data
     for item in initial.into_iter() {
         injector.push(item);
     }
 
+    // Create single scope to contain all workers
     let result : Vec<OUT> = crossbeam_utils::thread::scope(|scope|
     {   
-        let mut scopes : Vec<_> = Default::default();
+        // Container for all workers
+        let mut worker_scopes : Vec<_> = Default::default();
 
+        // Create each worker
         for worker in workers.into_iter() {
+            
+            // Make copy of data so we can move clones or references into closure
             let injector_borrow = &injector;
             let stealers_copy = stealers.clone();
             let job_copy = job.clone();
             let mut counter_copy = active_counter.clone();
 
+            // Create scope for single worker
             let s = scope.spawn(move |_| {
-                let backoff = crossbeam_utils::Backoff::new();
-
+                // results of this worker
                 let mut worker_results : Vec<_> = Default::default();
+
+                // backoff spinner for sleeping
+                let backoff = crossbeam_utils::Backoff::new();
 
                 // Loop until all workers idle
                 loop {
-                    // Do work
                     {
-                        let _work_token = counter_copy.take_token();
+                        // look for work
+                        let _ = counter_copy.take_token();
                         while let Some(item) = find_task(&worker, injector_borrow, &stealers_copy) {
                             backoff.reset();
 
+                            // do work
                             if let Some(result) = job_copy(item, &worker) {
                                 worker_results.push(result);
                             }
                         } 
                     }
 
-                    backoff.spin();
-
+                    // no work, check if all workers are idle
                     if counter_copy.is_zero() {
                         break;
                     }
+
+                    // sleep
+                    backoff.spin();
                 }
 
                 worker_results
             });
 
-            scopes.push(s);
+            worker_scopes.push(s);
         }
 
-        scopes.into_iter()
+        // run all workers to completion and combine their results
+        worker_scopes.into_iter()
             .filter_map(|s| s.join().ok())
             .flatten()
             .collect()
@@ -88,8 +103,41 @@ fn find_task<T>(
     })
 }
 
+
+
+// Helpers to track when all workers are done
+#[derive(Clone)]
+struct ActiveCounter {
+    active_count: Arc<AtomicUsize>
+}
+
+impl ActiveCounter {
+    pub fn take_token(&mut self) -> ActiveToken {
+        self.active_count.fetch_add(1, Ordering::SeqCst);
+        ActiveToken { active_count: self.active_count.clone() }
+    }
+
+    pub fn new() -> ActiveCounter {
+        ActiveCounter { active_count: Arc::new(AtomicUsize::new(0)) }
+    }
+
+    pub fn is_zero(&self) -> bool {
+        self.active_count.load(Ordering::SeqCst) == 0
+    }
+}
+
+struct ActiveToken {
+    active_count: Arc<AtomicUsize>
+}
+
+impl Drop for ActiveToken {
+    fn drop(&mut self) {
+        self.active_count.fetch_sub(1, Ordering::SeqCst);
+    }
+}
+
 #[test]
-fn basic_scheduler() {
+fn basic_recursive_job() {
     let data = vec![3];
     let job = |x, worker: &Worker<_>| -> Option<i32> {
         if x > 0 {
@@ -100,12 +148,12 @@ fn basic_scheduler() {
         }
     };
 
-    let result = run_job(data, job, 1);
+    let result = run_recursive_job(data, job, 1);
     assert_eq!(result, vec![6,4,2]);
 }
 
 #[test]
-fn bigger_scheduler() {
+fn bigger_recursive_job() {
     let data = vec![1,2,3,4,5,6,7,8,9,10];
     let job = |x, worker: &Worker<_>| -> Option<i32> {
         if x > 0 {
@@ -120,6 +168,6 @@ fn bigger_scheduler() {
         .map(|x| (x*(x+1)))
         .sum();
 
-    let result = run_job(data, job, 1);
+    let result = run_recursive_job(data, job, 1);
     assert_eq!(result.iter().sum::<i32>(), expected_result);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
 use anyhow::anyhow;
 use clap::{Arg, App};
-use crossbeam_deque::{Injector, Stealer, Worker};
+use crossbeam_deque::Worker;
 use ignore::gitignore::{Gitignore, GitignoreBuilder};
 use itertools::Itertools;
 use num_format::{Locale, ToFormattedString};
 use std::{env, fs};
 use std::path::PathBuf;
-use std::sync::Arc;
-use std::sync::atomic::{AtomicUsize, Ordering};
 use std::time::Instant;
 
 mod immutable_stack;
@@ -101,14 +99,16 @@ fn main() -> anyhow::Result<()> {
         ignore_tip = ignore_stack.push(ignore);
     }
 
-    let initial_data = vec![(ignore_tip, starting_dir)];
-    let job = |(mut ignore_tip, entry): (ImmutableStack<Gitignore>, PathBuf), worker: &Worker<_>| -> Option<Vec<PathBuf>> {
+    // Recursive job takes a path, checks if it's ignored, and recurses into subdirs if needed
+    // Return value is result for the path only. Sub-directories will run separately
+    // and return their own result.
+    let recursive_job = |(mut ignore_tip, path): (ImmutableStack<Gitignore>, PathBuf), worker: &Worker<_>| -> Option<Vec<PathBuf>> {
         
         let mut job_ignores : Vec<_> = Default::default();
         let mut dirs : Vec<std::path::PathBuf> = Default::default();
 
         // Process each child in directory
-        let read_dir = match fs::read_dir(entry) {
+        let read_dir = match fs::read_dir(path) {
             Ok(inner) => inner,
             Err(_) => return None,
         };
@@ -176,133 +176,81 @@ fn main() -> anyhow::Result<()> {
         Some(job_ignores)
     };
 
+    // Data for multi-threaded job
     let num_threads : usize = matches.value_of("num_threads").unwrap().parse().unwrap();
-    let job_result = job_system::run_job(initial_data, job, num_threads);
-    let ignored_paths : Vec<PathBuf> = job_result.into_iter()
+    let initial_data = vec![(ignore_tip, starting_dir)];
+
+    // Run recursive jobs
+    let ignored_paths = job_system::run_recursive_job(initial_data, recursive_job, num_threads)
+        .into_iter()
         .flatten()
         .collect();
-
-/*
-    // Create crossbeam structs for work-stealing job queue
-    let num_threads : usize = matches.value_of("num_threads").unwrap().parse().unwrap();
-    let injector = Injector::new();
-    let workers : Vec<_> = (0..num_threads).map(|_| Worker::new_lifo()).collect();
-    let stealers : Vec<_> = workers.iter().map(|w| w.stealer()).collect();
-    let active_counter = ActiveCounter::new();
     
-    // Seed injector
-    injector.push((ignore_tip, starting_dir));
+    // Second recursive job to compute size of ignored directories
+    let recursive_dir_size_job = |path: PathBuf, worker: &Worker<_>| -> Option<(PathBuf, u64)> {
+        // Get type of path
+        let path_meta = match fs::metadata(&path) {
+            Ok(meta) => meta,
+            Err(_) => return None
+        };
 
-    let ignored_paths : Vec<std::path::PathBuf> = crossbeam_utils::thread::scope(|scope|
-    {   
-        let mut scopes : Vec<_> = Default::default();
-
-        for worker in workers.into_iter() {
-            let injector_borrow = &injector;
-            let stealers_copy = stealers.clone();
-            let mut counter_copy = active_counter.clone();
-
-            let s = scope.spawn(move |_| {
-                let backoff = crossbeam_utils::Backoff::new();
-                let mut worker_ignores : Vec<_> = Default::default();
-
-                // Loop until all workers idle
-                loop {
-                    // Do work
-                    {
-                        let _work_token = counter_copy.take_token();
-                        while let Some((mut ignore_tip, entry)) = find_task(&worker, injector_borrow, &stealers_copy) {
-                            backoff.reset();
-
-                            || -> anyhow::Result<()> {
-                                // Dirs must be deferred so they can consider potential ignore files
-                                let mut dirs : Vec<std::path::PathBuf> = Default::default();
-
-                                // Process each child in directory
-                                let read_dir = match fs::read_dir(entry) {
-                                    Ok(inner) => inner,
-                                    Err(_) => return Ok(()),
-                                };
-
-                                for child in read_dir {
-                                    let child_path = child?.path();
-                                    let child_meta = match std::fs::metadata(&child_path) {
-                                        Ok(inner) => inner,
-                                        Err(_) => continue
-                                    };
-
-                                    // Child is file
-                                    if child_meta.is_file() {
-                                        // Child is ignorefile. Push it onto ignore stack
-                                        if child_path.file_name().unwrap() == ".gitignore" {
-                                            // Parse new .gitignore
-                                            let parent_path = child_path.parent()
-                                                .ok_or_else(|| anyhow!("Failed to get parent for [{:?}]", child_path))?;
-                                            
-                                            let mut ignore_builder = GitignoreBuilder::new(parent_path);
-                                            ignore_builder.add(child_path);
-                                            let new_ignore = ignore_builder.build()?;
-                                            ignore_tip = ignore_tip.push(new_ignore);
-                                        } else {
-                                            // Child is file. Perform ignore test.
-                                            let is_ignored = ignore_tip.iter()
-                                                .map(|i| i.matched(&child_path, true))
-                                                .any(|m| m.is_ignore());
-
-                                            if is_ignored {
-                                                worker_ignores.push(child_path);
-                                            }
-                                        }
-                                    } else {
-                                        // Child is directory. Add to deferred dirs list
-                                        dirs.push(child_path);
-                                    }
-                                }
-
-                                // Process directories
-                                for dir in dirs.into_iter() {
-
-                                    // Check if ignored
-                                    let is_ignored = ignore_tip.iter()
-                                        .map(|i| i.matched(&dir, true))
-                                        .any(|m| m.is_ignore());
-
-                                    if is_ignored {
-                                        worker_ignores.push(dir);
-                                    } else {
-                                        worker.push((ignore_tip.clone(), dir));
-                                    }
-                                }
-                                
-                                Ok(())
-                            }().unwrap();
-                        } 
-                    }
-
-                    backoff.spin();
-
-                    if counter_copy.is_zero() {
-                        break;
-                    }
-                }
-
-                worker_ignores
-            });
-
-            scopes.push(s);
+        // If file, return result immediately
+        if path_meta.is_file() {
+            return Some((path, path_meta.len()));
         }
 
-        let result : Vec<std::path::PathBuf> = scopes.into_iter()
-            .map(|s| s.join().unwrap())
-            .flatten()
-            .collect();
-        result
+        // Get director iterator
+        let read_dir = match fs::read_dir(&path) {
+            Ok(read_dir) => read_dir,
+            Err(_) => return None
+        };
 
-    }).unwrap();
-*/
+        // Iterate children
+        let mut files_size = 0;
+        for child in read_dir {
+            // Ignore errors
+            let child_path = match child {
+                Ok(entry) => entry.path(),
+                Err(_) => continue
+            };
+
+            let child_meta = match fs::metadata(&child_path) {
+                Ok(meta) => meta,
+                Err(_) => continue
+            };
+
+            // Accumualte file size
+            // Add directories to the worker
+            if child_meta.is_file() {
+                files_size += child_meta.len();
+            } else {
+                worker.push(child_path);
+            }
+        }
+        
+        Some((path, files_size))
+    };
+
+    // Compute path sizes
+    let final_ignore_paths = job_system::run_recursive_job(ignored_paths, recursive_dir_size_job, num_threads);
+
+    let final_ignore_paths : Vec<_> = final_ignore_paths.into_iter()
+        .filter(|(_, size)| *size >= min_filesize_in_bytes)
+        .sorted_by_key(|kvp| kvp.1)
+        .collect();
+
+    let mut total_bytes = 0;
+    for (path, bytes) in &final_ignore_paths {
+        total_bytes += bytes;
+        println!("  {:10} {:?}", pretty_bytes(*bytes), path);
+    }
+    println!("Total Bytes: {}", total_bytes.to_formatted_string(&Locale::en));
+    println!("Search Time: {:?}", start.elapsed());
+
 
     // Print all ignored content, sorted by bytes
     // TODO: parallelize with rayon
+    /*
     println!("Ignored:");
     let mut total_bytes = 0;
     let mut final_ignore_paths : Vec<_> = Default::default();
@@ -318,6 +266,7 @@ fn main() -> anyhow::Result<()> {
         });
     println!("Total Bytes: {}", total_bytes.to_formatted_string(&Locale::en));
     println!("Search Time: {:?}", start.elapsed());
+    */
 
     if final_ignore_paths.is_empty() {
         println!("No ignore paths to delete.");
@@ -370,7 +319,7 @@ fn main() -> anyhow::Result<()> {
             println!("\n☢️☢️☢️ nuclear launch detected ☢️☢️☢️");
 
             // Delete all the things
-            for path in final_ignore_paths {
+            for (path, _) in final_ignore_paths {
                 remove_path(&path);
             }
 
@@ -390,6 +339,7 @@ fn main() -> anyhow::Result<()> {
 }
 
 // Recursively calculate size of all files within a single directory
+/*
 fn dir_size(path: std::path::PathBuf) -> anyhow::Result<u64> {
     // Input path may be file
     let meta = fs::metadata(&path)?;
@@ -420,6 +370,7 @@ fn dir_size(path: std::path::PathBuf) -> anyhow::Result<u64> {
 
     Ok(result)
 }
+*/
 
 // Print u64 bytes value as a suffixed string
 fn pretty_bytes(orig_amount: u64) -> String {
@@ -444,56 +395,4 @@ fn pretty_bytes(orig_amount: u64) -> String {
     }
 }
 
-fn find_task<T>(
-    local: &Worker<T>,
-    global: &Injector<T>,
-    stealers: &[Stealer<T>],
-) -> Option<T> {
-    // Pop a task from the local queue, if not empty.
-    local.pop().or_else(|| {
-        // Otherwise, we need to look for a task elsewhere.
-        std::iter::repeat_with(|| {
-            // Try stealing a batch of tasks from the global queue.
-            global.steal_batch_and_pop(local)
-                // Or try stealing a task from one of the other threads.
-                .or_else(|| stealers.iter().map(|s| s.steal()).collect())
-        })
-        // Loop while no task was stolen and any steal operation needs to be retried.
-        .find(|s| !s.is_retry())
-        // Extract the stolen task, if there is one.
-        .and_then(|s| s.success())
-    })
-}
-
-// Helpers to track when all workers are done
-#[derive(Clone)]
-struct ActiveCounter {
-    active_count: Arc<AtomicUsize>
-}
-
-impl ActiveCounter {
-    pub fn take_token(&mut self) -> ActiveToken {
-        self.active_count.fetch_add(1, Ordering::SeqCst);
-        ActiveToken { active_count: self.active_count.clone() }
-    }
-
-    pub fn new() -> ActiveCounter {
-        ActiveCounter { active_count: Arc::new(AtomicUsize::new(0)) }
-    }
-
-    pub fn is_zero(&self) -> bool {
-        self.active_count.load(Ordering::SeqCst) == 0
-    }
-}
-
-
-struct ActiveToken {
-    active_count: Arc<AtomicUsize>
-}
-
-impl Drop for ActiveToken {
-    fn drop(&mut self) {
-        self.active_count.fetch_sub(1, Ordering::SeqCst);
-    }
-}
 
